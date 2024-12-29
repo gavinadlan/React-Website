69dc63c521ca520b86da4faa3c05d5990faeb0ca
gavin adlan hidayat <gavinadlan@gmail.com>
2024-12-30 02:34:58 +0700

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram"; // Ganti nama impor sesuai
+import TwitterIcon from "@mui/icons-material/Twitter"; // Ganti nama impor sesuai
+import FacebookIcon from "@mui/icons-material/Facebook"; // Ganti nama impor sesuai
+import LinkedInIcon from "@mui/icons-material/LinkedIn"; // Ganti nama impor sesuai
+import "../styles/Footer.css";
 
 function Footer() {
-  return <div>pizza</div>;
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+      </div>
+      <p> &copy; 2021 pedrotechpizza.com</p>
+    </div>
+  );
 }
 
 export default Footer;
