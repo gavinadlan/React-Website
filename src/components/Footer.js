ba3d7d4196b7eccf86c00a29fd016a1698f768a6
gavin adlan hidayat <gavinadlan@gmail.com>
2024-12-31 21:40:10 +0700

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,42 @@
 import React from "react";
-import InstagramIcon from "@mui/icons-material/Instagram"; // Ganti nama impor sesuai
-import TwitterIcon from "@mui/icons-material/Twitter"; // Ganti nama impor sesuai
-import FacebookIcon from "@mui/icons-material/Facebook"; // Ganti nama impor sesuai
-import LinkedInIcon from "@mui/icons-material/LinkedIn"; // Ganti nama impor sesuai
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterIcon />
+        </a>
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
       </div>
       <p> &copy; 2024 vinpizza.com</p>
     </div>
